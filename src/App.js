7a86b2f15cb73d68e5339225cda1df068b434108
javierskodnik <jskodnik@despegar.com>
2021-10-20 23:43:03 -0300

    subiendo hasta Eliminar Producto

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import Footer from "./components/Footer";
-import {useState, Fragment} from "react";
+import {useState } from "react";
+import { Fragment } from "react";
 import Header from "./components/Header";
 import Producto from "./components/Producto";
+import Changuito from "./components/Changuito";
 
 
 function App() {
@@ -10,33 +12,46 @@ function App() {
   //primero va la variable que yo voy a tocar, y luego de la "," va la función
   //que se encarga de modificar el estado inicial
 
-  const [productos, guardarProducto] = useState ([
+  const [productos, guardarProductos] = useState ([
     {id:1, articulo: "Zapatillas Para Tenis", precio: 8000},
     {id:2, articulo: "Camiseta Termica", precio: 2000},
     {id:3, articulo: "Palo de Hockey", precio: 4500},
     {id:4, articulo: "Botines Fútbol 5", precio: 10000},
     {id:5, articulo: "Calza", precio: 2500},
 
-  ])
+  ]);
+
+  //State para changuito
+
+  const [changuito, agregarProducto] = useState ([]);
 
   return (
     <Fragment className="App">
       <Header />
       <h1>Indumentaria Deportiva - Venta Online</h1>
-      {/*vamos a iterar sobre la lista de const producto */}
       
-      {productos.map( producto =>
+      {productos.map(producto =>
         (
           <Producto 
-          
-          producto = {producto}
-          
+            //para que cada producto sea único, se le asigna una key. Es el id del producto
+            key = {producto.id}
+            producto = {producto}
+
+            // vamos a pasar la lista de productos para poder hacer "seleccionar productos"
+            productos = {productos}
+
+            changuito = {changuito}
+            agregarProducto = {agregarProducto}
           />
         )
       )}
 
+      <Changuito
+        changuito={changuito}
+        agregarProducto = {agregarProducto}
       
-      {/*a Footer le vamos el año pasar como propiedad al componente Footer */}
+      />
+
       <Footer 
         anio= {2021}
       />
--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -1,9 +1,58 @@
 import React from 'react';
+import { Fragment } from 'react';
 
 
-const Producto = ({producto}) => {
+const Producto = ({producto, productos, changuito, agregarProducto}) => {
+
+    //vamos a poner una declaración para que cada vez que querramos usar la propiedad,
+    //no useamos producto.propiedad
+
+    const {id, articulo, precio} = producto;
+
+    //funcion para agregar productos al changuito
+    const seleccionarProducto = (id) => {
+        //vamos a quedarnos con el producto que tenga el id que viene por parametro
+        const producto = productos.filter(
+            producto => producto.id === id
+        )[0];
+        agregarProducto([...changuito, producto]);
+        //console.log(producto);
+        console.log(changuito);
+
+    }
+
+    //funciona para quitar productos del changuito
+    const eliminarProducto = (id) => {
+        //primero me quedo con el producto que no estoy borrando
+        //recorremos la lista del changuito y nos quedamos con los que no son los que queremos eliminar
+        const productos = changuito.filter( producto => producto.id !== id);
+        
+        //despues agregamos ese filtro
+        agregarProducto(productos);
+        //console.log("Quitando del changuito: ", id)
+
+        }
+    
     return ( 
-        <h3>{producto.id} {producto.articulo} {producto.precio}</h3>
+        <Fragment>
+            <div>
+                <h3>{id} {articulo} {precio}</h3>
+                {
+                    productos
+                    ?
+                        <button
+                            type="button"
+                            onClick={ () => seleccionarProducto(id)}
+                        >Comprar</button>
+                    :
+                        <button
+                            type="button"
+                            onClick={() => eliminarProducto(id)}
+                        >Eliminar</button>
+                }
+            </div>
+        </Fragment>
+        
      );
 }
  
